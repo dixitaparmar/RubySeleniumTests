66876dbe046d1cc39d7e2f15d52fa6b358744c5d
Dixita Parmar <dixita.parmar@bjss.com>
2015-01-19 14:54:59 +0000

    Changed the code to see if teamcity build kicks off

--- RubySeleniumTests/Example1/features/step_definitions/test1_steps.rb
+++ RubySeleniumTests/Example1/features/step_definitions/test1_steps.rb
@@ -5,7 +5,7 @@ require "selenium-webdriver"
 
 Given(/^I am on "(.*?)" page$/) do |path_to|
    $driver_ff.navigate.to path_to
-   print "test"
+   print "test to see if teamcity build kicks off"
 end
 
 When(/^I type "(.*?)" in search field$/) do |string_search| 
